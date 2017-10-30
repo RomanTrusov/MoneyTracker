2b6ceb0657cc7faac0ecdb4ea55aa7a1ae245cac
Roman Trusov <kreezzzfaqer@gmail.com>
2017-10-30 17:14:50 +1000

    Continue homework 2. Almost done. Only the shadow left.

--- app/src/main/java/com/loftschool/moneytracker/MainActivity.java
+++ app/src/main/java/com/loftschool/moneytracker/MainActivity.java
@@ -2,20 +2,59 @@ package com.loftschool.moneytracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    public boolean nameEntered = false, moneyEntered = false; //Вводим две переменные для проверки наличия текста в каждом окне
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText titleEdit = (EditText) findViewById(R.id.name); //Активируем ввод текста для кода
+        final EditText moneyEdit = (EditText) findViewById(R.id.money); //Активируем ввод денег для кода
+        final ImageButton addButton = (ImageButton) findViewById(R.id.add); //Активируем кнопку для кода
+        addButton.setEnabled(false); //Делаем кнопку неактивной здесь, потому что ImageButton не деактивируется через xml (только через android:clickable)
+
+
+        titleEdit.addTextChangedListener(new TextWatcher() { //Отслеживание текста в поле nameEdit
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged( CharSequence s, int start, int before, int count) {
+                nameEntered = !TextUtils.isEmpty(s); //Если во время ввода текста в строке что-то есть, то nameEntered = true
+                addButton.setEnabled(moneyEntered&&nameEntered); //кнопка активна, если обе переменные true
+        }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        moneyEdit.addTextChangedListener(new TextWatcher() { //Отслеживание текста в поле moneyEdit
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                moneyEntered = !TextUtils.isEmpty(s); //Если во время ввода текста в строке что-то есть, то moneyEntered = true
+                addButton.setEnabled(moneyEntered&&nameEntered); //кнопка активна, если обе переменные true
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
 
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-    }
+    protected void onStart() {super.onStart();}
 
     @Override
     protected void onResume() {
